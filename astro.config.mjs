c076b4b87e6a8d97c9b52733120a2a298fac4736
Nightstage <info@nightstage-es.com>
2026-04-09 00:21:12 +0200

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://nightstage.github.io",
+  base: "/NIGHTSTAGE-WEB",
   server: {
     host: true,
     port: 4321
